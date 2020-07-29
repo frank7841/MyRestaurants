c7a7c80bc2cf590170cdbb4b70b29f3c10828b14
frank7841 <franklinewax@gmail.com>
2020-07-29 17:55:10 +0300

    CLoses soft keyboard an test to confirm button works passes

---
+++ app/src/androidTest/java/com/moringaschool/myrestaurants/MainActivityInstrumentationTest.java
@@ -0,0 +1,44 @@
+package com.moringaschool.myrestaurants;
+import androidx.test.espresso.Espresso;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity>activityActivityTestRule=new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText(){
+        onView(withId(R.id.locationEditText)).perform(typeText("Namawanga"))
+                    .check(matches(withText("Namawanga")));
+    }
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String location = "Namawanga";
+        onView(withId(R.id.locationEditText)).perform(typeText(location)).perform(closeSoftKeyboard());
+        try {
+            Thread.sleep(250);
+        }
+        catch (InterruptedException e){
+            System.out.println("got Interrupted");
+        }
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("Here are all the Restaurants near: " + location)));
+    }
+}
--- app/src/test/java/com/moringaschool/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/moringaschool/myrestaurants/MainActivityTest.java
@@ -1,15 +1,40 @@
 package com.moringaschool.myrestaurants;
 
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(RobolectricTestRunner.class)
+@Config(maxSdk = Build.VERSION_CODES.P, minSdk = Build.VERSION_CODES.P) // Value of Build.VERSION_CODES.P is 28
 public class MainActivityTest {
     private MainActivity activity;
 
     @Before
     public void setUp(){
         activity = Robolectric.setupActivity(MainActivity.class);
+    }
+    @Test
+    public void validateTextViewContent(){
+        TextView appNmaeTextView = activity.findViewById(R.id.appNameTextView);
+        assertTrue("My Restaurants".equals(appNmaeTextView.getText().toString()));
+    }
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.findRestaurantsButton).performClick();
+        Intent expectedIntent = new  Intent(activity, RestaurantsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
