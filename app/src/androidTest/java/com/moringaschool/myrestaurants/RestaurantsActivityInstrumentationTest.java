95fcea5d36282607771ed76152a02712b73c8752
frank7841 <franklinewax@gmail.com>
2020-07-31 12:25:26 +0300

    logcat and debugging

--- app/src/androidTest/java/com/moringaschool/myrestaurants/RestaurantsActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -1,4 +1,40 @@
 package com.moringaschool.myrestaurants;
 
+import android.view.View;
+import androidx.test.rule.ActivityTestRule;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsAnything.anything;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+
 public class RestaurantsActivityInstrumentationTest {
+    @Rule
+     public ActivityTestRule<RestaurantsActivity> activityTestRule = new ActivityTestRule<>(RestaurantsActivity.class);
+
+    @Test
+    public void istItemClickDisplaysToastWithCorrectRestaurant(){
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String restaurantName = "Lilians";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
 }
--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -8,19 +8,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findRestaurantsButton)Button mFindRestaurantButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantButton;
-    private EditText mLocationEditText;
+//    private Button mFindRestaurantButton;
+//    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFindRestaurantButton = (Button)findViewById(R.id.findRestaurantsButton);
-        mLocationEditText = (EditText)findViewById(R.id.locationEditText);
+        ButterKnife.bind(this);
+//        mFindRestaurantButton = (Button)findViewById(R.id.findRestaurantsButton);
+//        mLocationEditText = (EditText)findViewById(R.id.locationEditText);
         mFindRestaurantButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
